44280d9ec8619b8cad1122a50c6980d05c659faa
obidjonjoraboyev <obidjonn007@icloud.com>
2024-02-16 22:58:32 +0500

    Last update code

---
+++ lib/utils/colors/app_colors.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+  static const Color transparent = Colors.transparent;
+
+  static const Color c_1A72DD = Color(0xFF1A72DD);
+  static const Color c_F4261A = Color(0xFFF4261A);
+  static const Color c_2A3256 = Color(0xFF2A3256);
+  static const Color c_C4C4C4= Color(0xFFC4C4C4);
+
+
+  static const Color c_273032= Color(0xFF273032);
+  static const Color c_162023= Color(0xFF162023);
+  static const Color c_2F3739= Color(0xFF2F3739);
+  static const Color c_F2F2F2= Color(0xFFF2F2F2);
+  static const Color c_F2954D= Color(0xFFF2954D);
+  static const Color c_BDBDBD= Color(0xFFBDBDBD);
+}
---
+++ lib/utils/images/app_images.dart
@@ -0,0 +1,15 @@
+class AppImages {
+  //.png
+  static const String boy = "assets/images/boy.png";
+  static const String teacher = "assets/images/img_1.png";
+
+
+  //.svg
+  static const String delete = "assets/icons/delete.svg";
+  static const String arrowBack = "assets/icons/arrow_back.svg";
+  static const String timer = "assets/icons/timer.svg";
+
+  //.jpg
+
+  //.gif
+}
